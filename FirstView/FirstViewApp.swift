75780147d9048c4f8bfd977db22c11b5f26ba8fd
Vijayarajan Govindarajan (VGovindarajan) <vg2131@gmail.com>
2025-02-12 07:47:04 -0600

    Initial Commit

---
+++ FirstView/FirstViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewApp.swift
+//  FirstView
+//
+//  Created by Vijayarajan Govindarajan on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstViewTests/FirstViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewTests.swift
+//  FirstViewTests
+//
+//  Created by Vijayarajan Govindarajan on 2/12/25.
+//
+
+import Testing
+@testable import FirstView
+
+struct FirstViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
